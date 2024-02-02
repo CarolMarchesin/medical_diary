bbd80ed1f7c05e9166ef9df735dd73a61f60d431
CarolMarchesin <104083173+CarolMarchesin@users.noreply.github.com>
2024-02-01 22:04:58 -0300

    feat: add coment date and name

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,19 @@
 import Image from "next/image";
 import Header from "../_components/header";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className="px-6 pt-6">
+        <h2 className="font-bold text-xl"> Olá, Carol!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", { locale: ptBR })}
+        </p>
+      </div>
     </div>
   );
 }
